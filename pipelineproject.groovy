3448d9b9cedd6de25f733ad1e9e907ab5ee2428f
Lewis <chieflouie@hotmail.co.uk>
2020-10-29 21:35:29 +0000

    New code.

--- pipelineproject.groovy
+++ pipelineproject.groovy
@@ -9,13 +9,12 @@ pipeline{
                 steps {
                     sshagent(['centostest1-root']) {
                     echo "-------------------------------------------------------------------------------------------------------"
-                    echo "                                        Running stage 1."    
+                    echo "                                        Sorting Directories"    
                     echo "-------------------------------------------------------------------------------------------------------"
                     sh '''
                     ssh root@$HOST1 'if [[ -d /opt/wildflybackup ]]; then echo "Directory already exists, moving on..."; else mkdir -v /opt/wildflybackup; echo "Directory now created."; fi'
                     ssh root@$HOST2 'if [[ -d /opt/wildflybackup ]]; then echo "Directory already exists, moving on..."; else mkdir -v /opt/wildflybackup; echo "Directory now created."; fi'
-                    ssh root@$HOST1 'cd /opt/wildflybackup' && 'curl --lewis-git "caspyin:SuperBambiLC2k16" https://github.com/lewis-git/jenkins/blob/master/backup001.tar
-                    '''
+                        '''
                     }
                 }
             }
@@ -23,10 +22,10 @@ pipeline{
                 steps {
                     sshagent(['centostest1-root']) {
                     echo "-------------------------------------------------------------------------------------------------------"
-                    echo "                                        Running stage 2, ignore..."
+                    echo "                                        Retrieving Backup TAR"
                     echo "-------------------------------------------------------------------------------------------------------"
                     sh '''
-                    #ssh root$HOST1 ls -al
+                    ssh root@$HOST1 'cd /opt/wildflybackup'; 'curl --lewis-git "caspyin:SuperBambiLC2k16" https://github.com/lewis-git/jenkins/blob/master/backup001.tar
 			        '''
                     }
                 }
